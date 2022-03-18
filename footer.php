c6ecc5e62c03d5024997edb74428680f3891a3f0
LakeTheImperial <96445516+LakeTheImperial@users.noreply.github.com>
2022-03-18 03:32:27 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
       <footer>
-        <p>&copy; Redst0ne 2012-<?php echo date("Y"); ?> &amp; &copy; Mizune 2019-<?php echo date("Y"); ?></p>
+        <p>&copy; Redst0ne 2012-<?php echo date("Y"); ?> &amp; &copy; Mizune 2019-<?php echo date("Y"); ?> (clipion beta preview)</p>
       </footer>
